133128b19a599783966aa1944ef9823adcfcd2f5
Deborah Vilas Boas <vvbadeborah@gmail.com>
2021-03-05 20:37:29 -0300

    Filtro na Tabela

--- src/components/tabela/index.js
+++ src/components/tabela/index.js
@@ -1,35 +1,67 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Tabela.css"
 
-export default function ({ titulo, objeto }) {
-   
-      const titulos = objeto.titulos
-      const linhas = objeto.linhas
+const Tabela = ({ titulo, objeto }) => {
+	const [filtro, setFiltro] = useState('');
+
+	let titulos = objeto.titulos;
+	let linhas = objeto.linhas;
+
+	if(filtro.length > 0) {
+		const linhasFiltradas = [];
+
+		linhas.forEach(linha => {
+			let linhaContemFiltro = false;
+
+			linha.forEach(coluna => {
+				if(String(coluna).indexOf(filtro) >= 0) {
+					linhaContemFiltro = true;
+				}
+			});
+
+			if(linhaContemFiltro)
+				linhasFiltradas.push(linha);
+		});
+
+		linhas = linhasFiltradas;
+	}
 
     return (
+		<>
+		<div className="filtro">
+			<span>Pesquise aqui</span>
+			<input
+				value={filtro}
+				onChange={ev => setFiltro(ev.target.value)}
+				placeholder="Filtro"
+			/>
+		</div>
+
         <div className="table">
-            <table border="1px" cellPadding="5px" cellSpacing="2" className="tabela">
-        <caption className="tituloTab">{titulo}</caption>
-        <thead>
-          
-          <tr>
-          {titulos.map((tit, i) =>(
-            <th key={i}>{tit}</th>
-          ))}
-          </tr>
-        </thead>
-        <tbody>
-          
-          {linhas.map((row, i) =>(
-            <tr className="td" key={i}>
-            {row.map((valor, j) =>(
-              <td key={j}>{valor}</td>
-            ))}
-            </tr>
-          ))}
-          
-        </tbody>
-      </table>
+			<table border="1px" cellPadding="5px" cellSpacing="2" className="tabela">
+			<caption className="tituloTab">{titulo}</caption>
+			
+			<thead>
+				<tr>
+					{titulos.map((tit, i) => (
+						<th key={i}>{tit}</th>
+					))}
+				</tr>
+			</thead>
+
+			<tbody>
+				{linhas.map((row, i) =>(
+					<tr className="td" key={i}>
+						{row.map((valor, j) => (
+							<td key={j}>{valor}</td>
+						))}
+					</tr>
+				))}
+			</tbody>
+			</table>
         </div>
+		</>
     )
 }
+
+export default Tabela;
